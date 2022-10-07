5405be0fe3df99ad90b5d8754d583da3e6bb3eac
matheusssilva991 <matheus.ssilva991@gmail.com>
2022-10-06 23:01:05 -0300

    "Alguns ajustes finais"

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# PROJETO-ORI
+# PROJETO ORI - Construção de Índice Invertido
 Projeto desenvolvido na disciplina Organização e recuperação de informação
+
+## Descrição
+Esse trabalho consiste na criação de um programa de gerenciamento de índices para um conjunto de arquivos no contexto de um sistema de Recuperação da Informação.
+
+## Entrada do programa
+O programa recebe como entrada o nome de três arquivos.txt. O primeiro arquivo consiste em um arquivo de texto que contém o conjunto de arquivos que serão indexados.
+O segundo arquivo especifica todas as palavras que serão desconsideradas dos arquivos do conjunto. O terceiro arquivo especifica as palavras que serão consultadas, sendo
+separadas por "," para consultar os arquivos que tenham ambas as palavras e por ";" para consultar os arquivos que tenham uma ou ambas as palavras.
+
+## Saida do programa
+O programa tem como saída dois arquivos.txt, arquivo de indice, contendo todas as palavras únicas contidas nos arquivos e quais arquivos elas pertencem, e o arquivo resposta, que contém quantos arquivos possuem as palavras consultadas e em quais arquivos elas estão.
--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 class arquivo():
     def __init__(self, nomeArquivo): # Construtor da classe
-        self.__nomeArquivo = nomeArquivo
-        self.__listaPalavras = []
-        self.__listaPalavrasDesconsideradas = []
+        self.__file_name = nomeArquivo
+        self.__list_words = []
+        self.__list_desconsidered_words = []
     # Fim do metodo construtor
     # Metodos getter --------------------------------------
-    def getNomeArquivo(self):
-        return self.__nomeArquivo
+    def get_file_name(self):
+        return self.__file_name
 
-    def getListaPalavrasDesconsideradas(self):
-        return self.__listaPalavrasDesconsideradas
+    def get_list_desconsidered_words(self):
+        return self.__list_desconsidered_words
 
-    def getListaPalavras(self):
-        return self.__listaPalavras
+    def get_list_words(self):
+        return self.__list_words
     # Fim dos metodos getter ------------------------------
 
   #Trata as palavras descondideradas
     def make_disconsidered_words(self, nomeArquivoDesconsideradas):
         with open(nomeArquivoDesconsideradas, 'r') as file:
-            for linha in file:
-                linha = linha.replace('\n', '')
+            for line in file:
+                line = line.replace('\n', '')
               #Salva na lista de palavras desconsideradas
-                self.__listaPalavrasDesconsideradas.append(linha)
+                self.__list_desconsidered_words.append(line)
       # end of method make_disconsidered_words
 
     #Preenche a lista de palavras         
@@ -31,73 +31,73 @@ class arquivo():
         if disconsidered != None:
             self.make_disconsidered_words(disconsidered)
 
-        with open(self.getNomeArquivo(), 'r') as file:
-            for linha in file:
-                palavras = []
+        with open(self.get_file_name(), 'r') as file:
+            for line in file:
+                words = []
 
-                linha = linha.replace('\n', '') 
+                line = line.replace('\n', '') 
                 for separador in separadores:
-                    linha = linha.replace(separador, '')#Troca por espaço em branco
+                    line = line.replace(separador, '')#Troca por espaço em branco
                   
-                palavras = linha.split(' ') # separa as palavras por espaço
-                for palavra in palavras:
-                    if self.__listaPalavrasDesconsideradas == []:
-                        self.__listaPalavras.append(palavra)
-                    elif palavra not in self.__listaPalavrasDesconsideradas:
-                        self.__listaPalavras.append(palavra)
+                words = line.split(' ') # separa as palavras por espaço
+                for word in words:
+                    if self.__list_desconsidered_words == []:
+                        self.__list_words.append(word)
+                    elif word not in self.__list_desconsidered_words:
+                        self.__list_words.append(word)
     # end of method make_words_file
 # end of class arquivo
 
 #Cria os objetos Arquivos para cada arquivo do conjunto
 def make_files(conjunto, desconsideradas = None):
     
-    arquivos = []
+    files = []
     
     with open(conjunto, 'r') as file:
-        ponteiroArquivo = 0
-        for nomeArquivo in file:
-            arquivos.append(arquivo(nomeArquivo.replace('\n', '')))
-            arquivos[ponteiroArquivo].make_words_file(desconsideradas.replace('\n', ''))
-            ponteiroArquivo += 1
+        ptr_file = 0
+        for file_name in file:
+            files.append(arquivo(file_name.replace('\n', '')))
+            files[ptr_file].make_words_file(desconsideradas.replace('\n', ''))
+            ptr_file += 1
             
-    return arquivos #retorna a lista dos objetos
+    return files #retorna a lista dos objetos
 # end of method make_files
 
 #Criadora do arquivo de índices
 def make_index(arquivos):
     #União com palavras únicas de cada arquivo
-    palavras = list(set(arquivos[0].getListaPalavras()) | set(arquivos[1].getListaPalavras()) | set(arquivos[2].getListaPalavras()))
-    palavras.sort() #Ordena
+    words = list(set(arquivos[0].get_list_words()) | set(arquivos[1].get_list_words()) | set(arquivos[2].get_list_words()))
+    words.sort() #Ordena
 
     with open("indice.txt", "w") as file: #Criação do arquivo de índices
-        for palavra in palavras:
-            linha = f"{palavra}: "
+        for word in words:
+            line = f"{word}: "
            #Em cada iteração verica em qual arquivo contém a palavra
           # e a quantidade de vezes que essa palavra ela se repete
             for i in range(3):
-                if palavra in arquivos[i].getListaPalavras():
-                    linha += f"{i + 1},{arquivos[i].getListaPalavras().count(palavra)} "
-            linha = linha.rstrip()
-            file.write(linha+"\n")
+                if word in arquivos[i].get_list_words():
+                    line += f"{i + 1},{arquivos[i].get_list_words().count(word)} "
+            line = line.rstrip()
+            file.write(line+"\n")
 
 # end of method make_index
 
 #Retorna como dicionário as linhas que contém as palavras de consulta
 def make_dict_index(lista_consulta):
-    dicionario_index = {}
+    dicionary_index = {}
 
     with open('indice.txt', 'r') as file:       
-        for linha in file:
-            linha_aux = linha.split(' ', 1)
-            linha_aux[1] = linha_aux[1].replace('\n', '')
-            linha_aux[0] = linha_aux[0].replace(':', '')
+        for line in file:
+            line_aux = line.split(' ', 1)
+            line_aux[1] = line_aux[1].replace('\n', '')
+            line_aux[0] = line_aux[0].replace(':', '')
             if (len(lista_consulta) == 1):
-                if lista_consulta[0] == linha_aux[0]:
-                    dicionario_index[linha_aux[0]] = linha_aux[1].replace('\n', '')
+                if lista_consulta[0] == line_aux[0]:
+                    dicionary_index[line_aux[0]] = line_aux[1].replace('\n', '')
             elif (len(lista_consulta) > 1):
-                if ((lista_consulta[0] == linha_aux[0]) or (lista_consulta[1] == linha_aux[0])): 
-                    dicionario_index[linha_aux[0]] = linha_aux[1].replace('\n', '')
-    return dicionario_index
+                if ((lista_consulta[0] == line_aux[0]) or (lista_consulta[1] == line_aux[0])): 
+                    dicionary_index[line_aux[0]] = line_aux[1].replace('\n', '')
+    return dicionary_index
 # end of method make_dict_index
 
 #Faz a consulta no arquivo consulta.txt
@@ -105,8 +105,8 @@ def get_answer(arquivo_consulta):
     consulta = None
     
     with open(arquivo_consulta.replace('\n', ''), 'r') as file:
-        for linha in file:
-            consulta = linha.replace('\n', '')
+        for line in file:
+            consulta = line.replace('\n', '')
 
     if ',' in consulta: #retorna and 
         operador = ','
@@ -135,7 +135,7 @@ def make_answer(arquivos, index_arquivos):
     with open("resposta.txt", 'w') as file:
         file.write(str(len(indexes))+"\n")
         for i in indexes:
-            file.write(arquivos[int(i) - 1].getNomeArquivo()+"\n")
+            file.write(arquivos[int(i) - 1].get_file_name()+"\n")
 # end of method make_answer  
 
 def main():
